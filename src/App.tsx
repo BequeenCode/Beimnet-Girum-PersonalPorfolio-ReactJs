4ec98158fbfce8132659d0b393612abccd531227
BequeenCode <girumbeimnet74@gmail.com>
2025-02-13 04:48:30 -0800

    modified files

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,10 @@ function App() {
     <>
         
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count + 1}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 class="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+        </div>
     </>
   )
 }
