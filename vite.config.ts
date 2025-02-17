a397cfdfcc651617753f2afb191a22ba39cfd803
BequeenCode <girumbeimnet74@gmail.com>
2025-02-17 02:50:22 -0800

    modified the vite configuration file

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
-
+  plugins: [],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(),
+        autoprefixer()
+      ]
+    }
+  }
 })
